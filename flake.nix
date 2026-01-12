2162c46e39f0e2aab50341cd1ccbb07f2cf5dc5d
Takafumi Asano <cariandrum22@gmail.com>
2026-01-12 19:54:19 +0900

    chore(release): 0.1.1

--- flake.nix
+++ flake.nix
@@ -338,7 +338,7 @@
         packages = {
           default = rustPlatform.buildRustPackage {
             pname = "claudius";
-            version = "0.1.0";
+            version = "0.1.1";
             src = ./.;
 
             cargoLock = {
